eb66e7f3fc07899b2601c15d975d6c2b65431976
Hein Htet Zaw <hhzheinhtetzaw31@gmail.com>
2019-09-30 10:34:31 +0630

    fix path

--- lib/rails_event_store_mongoid.rb
+++ lib/rails_event_store_mongoid.rb
@@ -1,7 +1,7 @@
 require 'rails_event_store/all'
 require 'rails_event_store_mongoid/event'
 require 'rails_event_store_mongoid/event_repository'
-require 'rails_event_store_mongoid/event_repository/event_repository_reader'
+require 'rails_event_store_mongoid/event_repository_reader'
 require 'rails_event_store_mongoid/version'
 
 # RailsEventStore.event_repository = RailsEventStoreMongoid::EventRepository.new
