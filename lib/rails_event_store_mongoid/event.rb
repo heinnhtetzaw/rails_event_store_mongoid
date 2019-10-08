ca85dd5899ecc7f204cdb85d3afa862e82d036f7
Hein Htet Zaw <heinhtetzaw@rebbiz.com>
2019-10-08 14:16:55 +0630

    temp change hash type to string and add indices

--- lib/rails_event_store_mongoid/event.rb
+++ lib/rails_event_store_mongoid/event.rb
@@ -10,21 +10,23 @@ module RailsEventStoreMongoid
     field :stream, type: String
     field :event_id, type: String
     field :event_type, type: String
-    field :meta, type: Hash, default: {}
-    field :data, type: Hash, default: {}
+    field :meta, type: String, default: ''
+    field :data, type: String, default: ''
 
     field :position, type: Integer
 
     field :ts, type: BSON::Timestamp, default: -> { BSON::Timestamp.new(0, 0) }
 
+    index({stream: 1, position: 1}, {unique: true})
+    index({stream: 1, event_id: 1}, {unique: true})
     index(event_id: 1)
-    index(stream: 1, ts: 1)
+    index({stream: 1, ts: 1}, {unique: true})
 
     def self.has_duplicate?(serialized_record, stream_name, linking_event_to_another_stream)
       if linking_event_to_another_stream
-        Event.where(id: serialized_record.event_id).count > 1
+        Event.where(event_id: serialized_record.event_id).count > 1
       else
-        Event.where(id: serialized_record.event_id, stream: stream_name).count > 0
+        Event.where(event_id: serialized_record.event_id, stream: stream_name).count > 0
       end
     end
   end
